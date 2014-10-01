200b787cf3a78503adaa8537ec051dffde2be1db
moxizhou <mzhou8@u.rochester.edu>
2014-09-30 19:07:15 -0700

    Refactored

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,37 +1,42 @@
-/* Import node's http module: */
-var http = require("http");
-// var handleRequest = require("./request-handler.js")
-// var url = require("url");
-
-var url = require("url");
-var messages = {results:[]};
 var express = require("express");
+var http = require("http");
 var app = express();
+var messages = {results:[]}; //put database in here later
+
+var statusCode = function(number, response) {
+  response.writeHead(number);
+};
+
+var getMessage = function(request, response) {
+  statusCode(200, response);
+  response.end(JSON.stringify(messages));
+};
+
+var sendMessage = function(request, response) {
+  statusCode(201, response);
+  request.on('data', function(data){
+    messages.results.push(JSON.parse(data));
+  })
+  response.end();
+};
+
+var error = function(request, response){
+  statusCode(404, response);
+  response.end();
+};
 
 app.use(function(request, response, next){
   response.header("access-control-allow-origin", "*");
   response.header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.header("access-control-allow-headers", "content-type, accept");
-  response.header("access-control-max-age", 10);
+  response.header("access-control-max-age", 30);
+  response.header("Content-Type", "text/plain");
   next();
 })
 
-app.get("/classes/messages", function(request, response) {
-  response.writeHead(200, {'Content-Type': "text/plain"});
-  response.end(JSON.stringify(messages));
-})
-app.post("/classes/messages", function(request, response) {
-  response.writeHead(201, {'Content-Type': "text/plain"});
-  request.on('data', function(data){
-    messages.results.push(JSON.parse(data));
-  });
-  response.end();
-})
-app.get("*", function(request, response){
-  response.writeHead(404, {'Content-Type': "text/plain"});
-  response.end();
-})
-
+app.get("/classes/messages", getMessage)
+app.post("/classes/messages", sendMessage)
+app.get("*", error)
 
 var port = 3000;
 var ip = "127.0.0.1";
